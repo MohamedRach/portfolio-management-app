a1e699f156f9f81f98ff1f8bfb19fa8db788913d
elmehdi <elmehdizarhouni@gmail.com>
2023-11-22 18:16:23 +0100

    Initial commit for test-mehdi branch

--- src/main/java/com/example/projets3/HelloServlet.java
+++ src/main/java/com/example/projets3/HelloServlet.java
@@ -11,33 +11,50 @@ import com.example.projets3.dao.UserDao.UserDao;
 import com.example.projets3.dao.UserDao.UserDaoImpl;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
-    private daoFactory factory;
     private UserDao userDao;
+
     public void init() {
         daoFactory dao_Factory = daoFactory.getInstance();
         this.userDao = new UserDaoImpl(dao_Factory);
     }
 
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-        //PersonDAO person = new PersonDAO();
         ArrayList<UserBean> users = this.userDao.getAllUsers();
-        // Hello
 
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        users.forEach(user -> {
-            out.println("<h1>" + user.getNom() + "</h1>");
-            out.println("<h1>" + user.getPrenom() + "</h1>");
-            out.println("<h1>" + user.getEmail() + "</h1>");
-        });
+        out.println("<h2>Liste des utilisateurs</h2>");
+        out.println("<form action=\"/createUtilisateur.jsp\" method=\"get\">");
+        out.println("<button class=\"btn btn-success float-right\" type=\"submit\">Créer</button></form>");
 
+        out.println("<table border=\"1\">");
+        out.println("<tr><th>Nom</th><th>Prénom</th><th>Email</th><th>Actions</th></tr>");
 
+        for (UserBean user : users) {
+            out.println("<tr>");
+            out.println("<td>" + user.getNom() + "</td>");
+            out.println("<td>" + user.getPrenom() + "</td>");
+            out.println("<td>" + user.getEmail() + "</td>");
+
+            // Ajouter les boutons "Modifier" et "Supprimer" avec des formulaires
+            out.println("<td>");
+            out.println("<form action=\"/modifier-utilisateur\" method=\"get\">");
+            out.println("<input type=\"hidden\" name=\"userId\" value=\"" + user.getId() + "\">");
+            out.println("<button type=\"submit\">Modifier</button></form>");
+
+            // Add a line break for better separation
+            out.println("<br>");
+
+            out.println("<form action=\"/supprimer-utilisateur\" method=\"post\">");
+            out.println("<input type=\"hidden\" name=\"userId\" value=\"" + user.getId() + "\">");
+            out.println("<input type=\"submit\" value=\"Supprimer\"></form>");
+            out.println("</td>");
+
+            out.println("</tr>");
+        }
+
+        out.println("</table>");
         out.println("</body></html>");
-    }
-
-    public void destroy() {
     }
 }
--- src/main/java/com/example/projets3/dao/UserDao/UserDaoImpl.java
+++ src/main/java/com/example/projets3/dao/UserDao/UserDaoImpl.java
@@ -59,22 +59,82 @@ public class UserDaoImpl implements UserDao {
     }
 
     @Override
+// UserDaoImpl.java
+
     public UserBean find(int id) throws DAOException {
-        return null;
+        UserBean user = null;
+        final String SQL_FIND_USER_BY_ID = "SELECT * FROM user WHERE id = ?";
+
+        try (Connection connection = daoFactory.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL_FIND_USER_BY_ID)) {
+
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                user = new UserBean();
+                user.setId(resultSet.getInt("id"));
+                user.setNom(resultSet.getString("nom"));
+                user.setPrenom(resultSet.getString("prenom"));
+                user.setEmail(resultSet.getString("email"));
+                // Ajoutez les autres propriétés de l'utilisateur si nécessaire
+            }
+
+        } catch (SQLException e) {
+            throw new DAOException("Erreur lors de la recherche de l'utilisateur par ID.", e);
+        }
+
+        return user;
     }
+
 
     @Override
     public void create(UserBean user) throws DAOException {
-        //hello
+        final String SQL_INSERT = "INSERT INTO user (nom, prenom, email) VALUES (?, ?, ?)";
+
+        try (Connection connexion = daoFactory.getConnection();
+             PreparedStatement preparedStatement = initRequestPrepare(connexion, SQL_INSERT, user.getNom(), user.getPrenom(), user.getEmail())) {
+
+            int statut = preparedStatement.executeUpdate();
+            if (statut == 0) {
+                throw new DAOException("Échec de la création de l'utilisateur, aucune ligne ajoutée dans la table.");
+            }
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
     }
 
     @Override
     public void update(UserBean user) throws DAOException {
-        //
+        final String SQL_UPDATE = "UPDATE user SET nom = ?, prenom = ?, email = ? WHERE id = ?";
+
+        try (Connection connexion = daoFactory.getConnection();
+             PreparedStatement preparedStatement = initRequestPrepare(connexion, SQL_UPDATE, user.getNom(), user.getPrenom(), user.getEmail(), user.getId())) {
+
+            int statut = preparedStatement.executeUpdate();
+            if (statut == 0) {
+                throw new DAOException("Échec de la mise à jour de l'utilisateur, aucune ligne modifiée dans la table.");
+            }
+        } catch (SQLException e) {
+            throw new DAOException("Erreur lors de la mise à jour de l'utilisateur.", e);
+        }
     }
+
 
     @Override
     public void delete(int id) throws DAOException {
-        //
+        final String SQL_DELETE = "DELETE FROM user WHERE id = ?";
+
+        try (Connection connexion = daoFactory.getConnection();
+             PreparedStatement preparedStatement = initRequestPrepare(connexion, SQL_DELETE, id)) {
+
+            int statut = preparedStatement.executeUpdate();
+            if (statut == 0) {
+                throw new DAOException("Échec de la suppression de l'utilisateur, aucune ligne supprimée dans la table.");
+            }
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
     }
+
 }
